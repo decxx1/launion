9d652b4c7962dffa28313270e69ceedd3bb7510f
Damian Cisternas <decxx1@gmail.com>
2026-07-11 17:43:26 -0300

    Enhance Astro configuration by adding new environment variables for schedules, phone, address, and map details. Update FloatingWhatsApp and Footer components to utilize these variables for improved contact information display. Change Layout background color for better visual contrast.

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,12 @@ export default defineConfig({
   integrations: [react(), sitemap()],
   env: {
     schema: {
+      SCHEDULES: envField.string({ context: "server", access: "public", optional: true }),
+      PHONE: envField.string({ context: "server", access: "public", optional: true }),
+      PHONE_FORMAT: envField.string({ context: "server", access: "public", optional: true }),
+      ADDRESS: envField.string({ context: "server", access: "public", optional: true }),
+      MAP_IFRAME: envField.string({ context: "server", access: "public", optional: true }),
+      MAP_URL: envField.string({ context: "server", access: "public", optional: true }),
       WHATSAPP_PHONE: envField.string({ context: "client", access: "public", optional: true }),
       EMAIL: envField.string({ context: "client", access: "public", optional: true }),
       SECRET_KEY: envField.string({ context: "client", access: "public", optional: true }),
